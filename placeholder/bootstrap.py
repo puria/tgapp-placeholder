32ae4221ed82616d51e96afe2d989a1c3afa3ae0
Marco Bosio <marco.bosio@axant.it>
2018-03-27 17:45:48 +0200

    removed unused models

--- placeholder/bootstrap.py
+++ placeholder/bootstrap.py
@@ -2,8 +2,5 @@
 """Setup the placeholder application"""
 from __future__ import print_function
 
-from placeholder import model
-from tgext.pluggable import app_model
-
 def bootstrap(command, conf, vars):
     print('Bootstrapping placeholder...')
